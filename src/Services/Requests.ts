faf50b6ffaf16ad1006fe9b70feed6e16256e90b
Dawood Ahmad <dawoodahmad@dawoods-mbp.lan>
2020-01-14 20:59:42 +0100

    request types

---
+++ src/Services/Requests.ts
@@ -0,0 +1,44 @@
+export const getRequest = (url: string) => {
+	const options = {
+		method: 'GET',
+		headers: {
+			'Content-type': 'application/json;'
+		}
+	};
+
+	return fetch(url, options)
+		.then(response => {
+			if (response.ok) {
+				return response.json();
+			} else {
+				throw new Error('Error fetching data...');
+			}
+		})
+		.then((responseData: any) => {
+			return responseData;
+		})
+		.catch(err => {
+			console.error(err);
+		});
+};
+
+export const deleteRequest = (url: string) => {
+	const options = {
+		method: 'DELETE',
+	};
+
+	return fetch(url, options)
+		.then(response => {
+			if (response.ok) {
+				return response.json();
+			} else {
+				throw new Error('Error fetching data...');
+			}
+		})
+		.then((responseData: any) => {
+			return responseData;
+		})
+		.catch(err => {
+			console.error(err);
+		});
+};
